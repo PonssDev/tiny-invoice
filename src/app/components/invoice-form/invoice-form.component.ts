7ca2827fb424c292f524de217ee622da9d18b6d6
pons <alanponstrabajo@gmail.com>
2025-04-27 17:00:41 +0200

    feat: default date and form outside the constructor

--- src/app/components/invoice-form/invoice-form.component.ts
+++ src/app/components/invoice-form/invoice-form.component.ts
@@ -3,7 +3,6 @@ import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } fr
 import { NgFor } from '@angular/common';
 import { InvoiceForm } from '../../interfaces/invoice-form-interface';
 import { FormService } from '../../services/form.service';
-import { InvoicePreview } from '../../interfaces/invoice-preview';
 
 @Component({
   selector: 'app-invoice-form',
@@ -18,7 +17,7 @@ import { InvoicePreview } from '../../interfaces/invoice-preview';
 export class InvoiceFormComponent implements OnInit {
   @Output() donwloadInvoice = new EventEmitter<void>()
 
-  public invoiceForm: FormGroup<InvoiceForm>;
+  public invoiceForm!: FormGroup<InvoiceForm>;
 
   public subtotal: number = 0;
 
@@ -32,8 +31,13 @@ export class InvoiceFormComponent implements OnInit {
 
   public taxAmount: number = 0;
 
+  public today: string = ''
+
   
   ngOnInit(): void {
+    this.getToday()
+    this.initForm()
+    console.log(this.today)
     this.invoiceForm.get('taxes.iva')?.valueChanges.subscribe(() => {
       this.calculateTotals();
     });
@@ -46,37 +50,7 @@ export class InvoiceFormComponent implements OnInit {
   constructor(
     private readonly fb: FormBuilder,
     private readonly formService: FormService,
-  ) {
-    this.invoiceForm = this.fb.group({
-      emisor: this.fb.group({
-        emisorName: this.fb.control('', Validators.required),
-        emisorAddress: this.fb.control(''), 
-        emisorNif: this.fb.control(''),
-        emisorEmail: this.fb.control(''),
-      }),
-      reciver: this.fb.group({
-        reciverName: this.fb.control('', Validators.required),
-        reciverAddress: this.fb.control(''),
-        reciverNif: this.fb.control(''),
-      }),
-      invoiceDetails: this.fb.group({
-        number: this.fb.control(''),
-        date: this.fb.control(''),
-        expirationDate: this.fb.control(''),
-      }),
-      services: this.fb.array([
-        this.newService()
-      ]),
-      taxes: this.fb.group({
-        iva: this.fb.control(21, Validators.required),
-        discount: this.fb.control<number | null>(null),
-      }),
-      additionalRemarks: this.fb.group({
-        remarks: this.fb.control(''),
-        paymentInformation: this.fb.control(''),
-      }),
-    });
-  }
+  ) {}
 
   get services(): FormArray {
     return this.invoiceForm.get('services') as FormArray;
@@ -108,6 +82,46 @@ export class InvoiceFormComponent implements OnInit {
 
   public onDownloadClick(): void{
     this.donwloadInvoice.emit()
+  }
+
+  private getToday(): void{
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = (now.getMonth() + 1).toString().padStart(2, '0')
+    const day = now.getDate().toString().padStart(2, '0')
+    this.today = `${year}-${month}-${day}`
+  }
+
+  private initForm(): void{
+    this.invoiceForm = this.fb.group({
+      emisor: this.fb.group({
+        emisorName: this.fb.control('', Validators.required),
+        emisorAddress: this.fb.control('', Validators.required), 
+        emisorNif: this.fb.control('', Validators.required),
+        emisorEmail: this.fb.control('', [Validators.required, Validators.email]),
+      }),
+      reciver: this.fb.group({
+        reciverName: this.fb.control('', Validators.required),
+        reciverAddress: this.fb.control('', Validators.required),
+        reciverNif: this.fb.control('', Validators.required),
+      }),
+      invoiceDetails: this.fb.group({
+        number: this.fb.control('', Validators.required),
+        date: this.fb.control(this.today),
+        expirationDate: this.fb.control(this.today),
+      }),
+      services: this.fb.array([
+        this.newService()
+      ]),
+      taxes: this.fb.group({
+        iva: this.fb.control(21, Validators.required),
+        discount: this.fb.control<number | null>(null),
+      }),
+      additionalRemarks: this.fb.group({
+        remarks: this.fb.control(''),
+        paymentInformation: this.fb.control(''),
+      }),
+    });
   }
 
   private newService(): FormGroup {
