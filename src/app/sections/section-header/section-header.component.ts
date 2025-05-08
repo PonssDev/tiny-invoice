f753914417d9bb16733e62c4b521103df7625061
PonssDev <alanponstrabajo@gmail.com>
2025-05-08 11:53:15 +0200

    feat: responsive

--- src/app/sections/section-header/section-header.component.ts
+++ src/app/sections/section-header/section-header.component.ts
@@ -7,7 +7,25 @@ import { RouterLink } from '@angular/router';
     RouterLink,
   ],
   templateUrl: './section-header.component.html',
-  styleUrl: './section-header.component.css'
+  styleUrl: './section-header.component.css',
+  standalone: true
 })
 export class SectionHeaderComponent {
+  menuActive = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+
+    // Evitar scroll cuando el menú está abierto
+    if (this.menuActive) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMenu() {
+    this.menuActive = false;
+    document.body.style.overflow = '';
+  }
 }
